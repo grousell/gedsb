72d84ca11e51bcc9f463d2d8e8aba7e0c9560d95
grousell <grousell@gmail.com>
2022-07-29 10:14:51 -0400

    Add board colours and flipped bar chart theme

---
+++ R/gedsb_colours.R
@@ -0,0 +1,17 @@
+
+# gedbs colours_fonts -----------------------------------------------------
+
+extrafont::loadfonts(device = "win")
+
+gedsbGreen <- "#8dc63f"
+gedsbGreen2 <- "#afd778"
+gedsbGreen3 <- "#d1e8b2"
+
+gedsbBlue <- "#0089cf"
+gedsbBlue2 <- "#4cacdd"
+gedsbBlue3 <- "#99cfeb"
+
+gedsbOrange <- "#faa61a"
+gedsbOrange2 <- "#fbc05e"
+gedsbOrange3 <- "fddba3"
+
