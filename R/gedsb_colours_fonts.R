23e460ae375bfbcf15ae8366f38d00c60a901381
grousell <grousell@gmail.com>
2022-09-13 12:45:02 -0400

    Combine load_fonts and load_color

---
+++ R/gedsb_colours_fonts.R
@@ -0,0 +1,39 @@
+
+#' gedsb_colours
+#'
+#' @param ...
+#'
+#' @return
+#' R objects with names Hex colours for GEDSB visual identity
+#' @export
+#'
+#' @examples
+#' gedsb_colours_fonts()
+#'
+
+gedsb_colours_fonts <- function (...){
+  gedsbGreen <<- "#8dc63f"
+  gedsbGreen2 <<- "#afd778"
+  gedsbGreen3 <<- "#d1e8b2"
+
+  gedsbBlue <<- "#0089cf"
+  gedsbBlue2 <<- "#4cacdd"
+  gedsbBlue3 <<- "#99cfeb"
+
+  gedsbOrange <<- "#faa61a"
+  gedsbOrange2 <<- "#fbc05e"
+  gedsbOrange3 <<- "fddba3"
+
+  extrafont::loadfonts(device="win")
+}
+
+
+
+
+
+
+
+
+
+
+
